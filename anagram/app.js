4c11b418b9c608d15a6cd7e1eb06d3905d574bef
Dale Holstein <holstein30@live.com>
2018-09-20 22:57:16 -0400

    anagram

--- anagram/app.js
+++ anagram/app.js
@@ -1,1 +1,37 @@
 // Check whether two strings are anagram of each other
+function split(a, b) {
+  anagram(a.split("").sort(), b.split("").sort());
+}
+
+function anagram(a, b) {
+  let iterate = 0;
+  if (a.length !== b.length) {
+    console.log(false);
+  } else {
+    a.forEach((x, i) => {
+      if (a[i] === b[i]) {
+        iterate++;
+      }
+    });
+  }
+  if (a.length === iterate) {
+    console.log(true);
+  } else {
+    console.log(false);
+  }
+}
+
+const checkAnagram = (word, anagram) => {
+  return (
+    anagram
+      .toString()
+      .toLowerCase()
+      .split("")
+      .join(" ") ===
+    word
+      .toString()
+      .toLowerCase.split("")
+      .sort()
+      .join(" ")
+  );
+};

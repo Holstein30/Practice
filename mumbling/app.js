9fbddc1eafc4f8adbd533dd6fdddf3b4b86c8760
Dale Holstein <holstein30@live.com>
2019-04-19 20:06:44 -0400

    mumbling splitting into array and looping

---
+++ mumbling/app.js
@@ -0,0 +1,13 @@
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+// The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+function accum(param) {
+  let parArray = param.split("");
+  parArray.forEach((letter, i) => {
+    console.log(letter, i);
+  });
+}
+
+accum("abcd");

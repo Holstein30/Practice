a3d2f932d8e6439db1c9a4dee4fe9159698d3f6e
Dale Holstein <holstein30@live.com>
2019-06-08 23:05:22 -0400

    factorial solved

--- factorial/app.js
+++ factorial/app.js
@@ -1,1 +1,16 @@
 // Return a number that is the factorial of the given number.
+
+let factorial = num => {
+  if (num === 0) {
+    return 1;
+  }
+  let sum = 1;
+  for (let i = 1; i <= num; i++) {
+    sum *= i;
+  }
+  return sum;
+};
+
+console.log(factorial(0)); // 1
+console.log(factorial(1)); // 1
+console.log(factorial(6)); // 720

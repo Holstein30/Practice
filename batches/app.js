a3ecac3eaeb9a94afd1e1016ba19b073940c3d42
Dale Holstein <holstein30@live.com>
2019-06-09 14:25:40 -0400

    batches SOLVED

--- batches/app.js
+++ batches/app.js
@@ -8,14 +8,29 @@ Each ingredient's value is number representing how many units there are.
 */
 
 let batches = (recipe, available) => {
-  const recipeArray = Object.keys(recipe);
-  const availableArray = Object.keys(available);
-
-  console.log(recipeArray);
-  console.log(availableArray);
+  let result = Math.floor(
+    Math.min(...Object.keys(recipe).map(k => available[k] / recipe[k] || 0))
+  );
+  console.log(result);
 };
 
 batches(
   { milk: 100, butter: 50, flour: 5 },
   { milk: 132, butter: 48, flour: 51 }
+);
+batches(
+  { milk: 100, flour: 4, sugar: 10, butter: 5 },
+  { milk: 1288, flour: 9, sugar: 95 }
+);
+
+// 1 batch can be made
+batches(
+  { milk: 100, butter: 50, cheese: 10 },
+  { milk: 198, butter: 52, cheese: 10 }
+);
+
+// 2 batches can be made
+batches(
+  { milk: 2, sugar: 40, butter: 20 },
+  { milk: 5, sugar: 120, butter: 500 }
 );

7a9f5aee14bc892550bbc8a195e8ffec955e216b
Dale Holstein <holstein30@live.com>
2019-06-09 14:05:38 -0400

    batches challenge added - unsolved

---
+++ batches/app.js
@@ -0,0 +1,8 @@
+// Create a function batches that returns the maximum number of whole batches that can be cooked from a recipe.
+/*
+It accepts two objects as arguments: the first object is the recipe
+for the food, while the second object is the available ingredients.
+Each ingredient's value is number representing how many units there are.
+
+`batches(recipe, available)`
+*/

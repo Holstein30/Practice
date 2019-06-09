cda26a0b8378f77a0531dae780e3b9ba595fc868
Dale Holstein <holstein30@live.com>
2019-06-09 14:17:29 -0400

    objects to arrays

--- batches/app.js
+++ batches/app.js
@@ -6,3 +6,16 @@ Each ingredient's value is number representing how many units there are.
 
 `batches(recipe, available)`
 */
+
+let batches = (recipe, available) => {
+  const recipeArray = Object.keys(recipe);
+  const availableArray = Object.keys(available);
+
+  console.log(recipeArray);
+  console.log(availableArray);
+};
+
+batches(
+  { milk: 100, butter: 50, flour: 5 },
+  { milk: 132, butter: 48, flour: 51 }
+);

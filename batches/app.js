0d9fcebda8c047a347ac3673e019f0e123d754c4
Dale Holstein <holstein30@live.com>
2019-06-09 14:26:17 -0400

    batches SOLVED

--- batches/app.js
+++ batches/app.js
@@ -14,10 +14,13 @@ let batches = (recipe, available) => {
   console.log(result);
 };
 
+// 0 batches can be made
 batches(
   { milk: 100, butter: 50, flour: 5 },
   { milk: 132, butter: 48, flour: 51 }
 );
+
+// 0 batches can be made
 batches(
   { milk: 100, flour: 4, sugar: 10, butter: 5 },
   { milk: 1288, flour: 9, sugar: 95 }

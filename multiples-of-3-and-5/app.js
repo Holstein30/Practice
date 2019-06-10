51601883615e9b27b3fc3a269faa5cb9e47d7b14
Dale Holstein <holstein30@live.com>
2019-06-10 15:26:46 -0400

    inital loop setup

--- multiples-of-3-and-5/app.js
+++ multiples-of-3-and-5/app.js
@@ -1,2 +1,10 @@
 // Project Euler Problem 1
 // Find the sum of all multiples of 3 or 5 below 1000
+
+let multiples = () => {
+  for (let i = 0; i < 1001; i++) {
+    console.log(i);
+  }
+};
+
+multiples();

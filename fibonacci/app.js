944d45842db26d2dd00d25d81c302ee5ae3caed6
Dale Holstein <holstein30@live.com>
2019-06-10 16:09:27 -0400

    getting answer = infinity for some reason when loop up to 4 million - currently debugging

--- fibonacci/app.js
+++ fibonacci/app.js
@@ -1,16 +1,16 @@
 // Project Euler problem 2
 // By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms
 
-// First need an equation to calculate Fibonacci numbers - i think its (n1 - 1) + (n2 + 1)
-// Maybe set up variables to be manipulated before loop??
-// ex: 1, 2, 3, 5, 8, 13, ...
+// Fib. sequence ex: 1, 2, 3, 5, 8, 13, ...
+// Whats the equation to calculate Fibonacci numbers? - its something like (n1 - 1) + (n2 + 1) or (i - 1) + (i - 2)??
+// Maybe set up variables to be manipulated before loop so i don't  need the equation?
 
 const fibonacci = () => {
   let first = 0;
   let second = 1;
   let result = 0;
 
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < 4000000; i++) {
     const sum = first + second;
     if (sum % 2 === 0) {
       result += sum;

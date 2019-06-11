bca756d44215941f234eeb1db633426c35b7d2eb
Dale Holstein <holstein30@live.com>
2019-06-11 13:59:11 -0400

    first test

--- prime-factor/app.js
+++ prime-factor/app.js
@@ -1,2 +1,19 @@
 // Project Euler problem 3
 // What is the largest prime factor of the number 600851475143
+
+// Prime = Only divisible by itself and one
+// Factors = number that goes into another number (ie: 5 is a factor of 25)
+
+// First need to write a function that determines all prime factors of a given number -
+
+let primeFactors = [];
+
+let getPrimeFactors = num => {
+  let emptyArray = new Array(num);
+
+  emptyArray.forEach(i => {
+    console.log(i);
+  });
+};
+
+getPrimeFactors(10);

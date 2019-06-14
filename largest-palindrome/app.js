45b5b2850c34720da22b19c59e109572a507e721
Dale Holstein <holstein30@live.com>
2019-06-14 18:10:05 -0400

    changed for loop so it only executes 3 digit numbers - different answer returned now but still not quite correct

--- largest-palindrome/app.js
+++ largest-palindrome/app.js
@@ -23,14 +23,10 @@ const isPalindrome = num => {
 // Need products for all pairs of 2 3-digit numbers that are palindromes
 
 const threeDigitProduct = () => {
-  for (let i = 999; i > 0; ) {
-    for (let j = 999; j > 0; j--) {
+  for (let i = 999; i > 99; i--) {
+    for (let j = 999; j > 99; j--) {
       if (isPalindrome(i * j)) {
         return `${i} times ${j} = ${i * j} which is a Palindrome`;
-      }
-      if (j === 1) {
-        i--;
-        j = 999;
       }
     }
   }

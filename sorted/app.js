ddcc664b3414c0e717f498b0ae84db1076f2e0b5
Dale Holstein <holstein30@live.com>
2019-06-15 18:52:39 -0400

    isSorted SOLVED

--- sorted/app.js
+++ sorted/app.js
@@ -1,1 +1,23 @@
 // isSorted - Returns true or false, indicating whether the given array of numbers is sorted.
+
+const isSorted = nums => {
+  let previousNum;
+  let sortBool = true;
+  nums.forEach(num => {
+    if (previousNum) {
+      if (previousNum > num) {
+        return (sortBool = false);
+      } else {
+        previousNum = num;
+      }
+    } else {
+      previousNum = num;
+    }
+  });
+  console.log(sortBool);
+};
+
+isSorted([]); // true
+isSorted([-Infinity, -5, 0, 3, 9]); // true
+isSorted([3, 9, -3, 10]); // false
+isSorted([20, 21, 22, 22, 24, 90]); // true

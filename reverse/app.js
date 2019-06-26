7404e79dccfdf6aa5d84208ccabedba1055b29ee
Dale Holstein <holstein30@live.com>
2019-06-26 14:34:57 -0400

    reverse setup

---
+++ reverse/app.js
@@ -0,0 +1,12 @@
+// Reverse a given string without using .reverse
+
+// first need to split string into an array
+// next create an empty array that will hold the reversed array
+// loop through the string array backwards and push each item into the empty arry
+// finally convert array back to a string and log it
+
+function reverse(string) {
+  const stringArr = string.split("");
+}
+
+reverse("abcdef");

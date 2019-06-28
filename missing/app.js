1f11e031f074dc5016259b6f41a9ac51f4099657
Dale Holstein <holstein30@live.com>
2019-06-28 07:21:15 -0400

    missing setup - sorting array

--- missing/app.js
+++ missing/app.js
@@ -1,5 +1,9 @@
 // Take an unsorted array of unique numbers (ie. no repeats) from 1 through some number n, and return the missing number in the sequence (there are either no missing numbers, or exactly one missing number).
 
+function missing(arr) {
+  const sortedArr = arr.sort();
+  console.log(sortedArr);
+}
 // First need to sort the array
 // Create a variable to hold the missing number
 // Next loop through and compare numbers to indexes and determine if a number is missing

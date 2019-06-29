54a4680d5ec2996dc8a40d8b67677ae3ab02ccd1
Dale Holstein <holstein30@live.com>
2019-06-29 10:10:49 -0400

    correctly filtering array so that only curly brackets remain

--- isBalanced/app.js
+++ isBalanced/app.js
@@ -1,4 +1,16 @@
 //  Take a string and return true or false indicating whether its curly braces are balanced
+// First split variable into an array
+// Next filter array so that only the curly brackets are left
+// Then I can loop through and first check if there is the correct count - if not return false
+// If count is correct I would check that the order is correct based on the count - this will return final answer
+
+function isBalanced(variable) {
+  const splitArray = variable.split("");
+  const curlyArray = splitArray.filter(
+    element => element === "{" || element === "}"
+  );
+  console.log(curlyArray);
+}
 
 isBalanced("}{"); // false
 isBalanced("{{}"); // false

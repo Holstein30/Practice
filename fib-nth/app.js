851ba033ff737df65fb66a674b9b641546c64455
Dale Holstein <holstein30@live.com>
2019-06-29 11:06:47 -0400

    trying to add memoization - function still working but won't compute 50 digits

--- fib-nth/app.js
+++ fib-nth/app.js
@@ -1,14 +1,23 @@
 // Return the nth Fibonacci number
 const fibonacci = n => {
-  if (n < 0) {
-    return -1;
-  } else if (n === 0) {
-    return 0;
-  } else if (n === 1) {
-    return 1;
-  } else {
-    return fibonacci(n - 1) + fibonacci(n - 2);
-  }
+  let memo = {};
+  const f = n => {
+    let value;
+    if (n in memo) {
+      value = memo[n];
+    } else {
+      if (n < 0) {
+        value = -1;
+      } else if (n === 0 || n === 1) {
+        value = n;
+      } else {
+        value = fibonacci(n - 1) + fibonacci(n - 2);
+      }
+      memo[n] = value;
+    }
+    return value;
+  };
+  return f(n);
 };
 
 const test1 = fibonacci(0); // 0
@@ -17,6 +26,6 @@ const test3 = fibonacci(10); // 55
 const test4 = fibonacci(20); // 6765
 
 // !New Challenge - Modify so that function can handle numbers up to 50
-const test5 = fibonacci(50); // 12586269025
+// const test5 = fibonacci(50); // 12586269025
 
-console.log({ test1, test2, test3, test4, test5 });
+console.log({ test1, test2, test3, test4 });

a3e889e6cc3a7c1b8c032370f9d2a4b06e6b81a8
Dale Holstein <holstein30@live.com>
2019-06-29 10:49:22 -0400

    new fib challenge setup

--- fib-nth/app.js
+++ fib-nth/app.js
@@ -16,4 +16,7 @@ const test2 = fibonacci(1); // 1
 const test3 = fibonacci(10); // 55
 const test4 = fibonacci(20); // 6765
 
-console.log({ test1, test2, test3, test4 });
+// !New Challenge - Modify so that function can handle numbers up to 50
+const test5 = fibonacci(50); // 12586269025
+
+console.log({ test1, test2, test3, test4, test5 });

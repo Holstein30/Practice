02aabcfa9918342a7edc522bdbab30390f9adf84
Dale Holstein <holstein30@live.com>
2019-07-03 15:22:58 -0400

    sort function start

--- sort/app.js
+++ sort/app.js
@@ -1,1 +1,12 @@
 //  Implement the sort function to sort an array of numbers
+// First try it with a bubble sort
+// Will need a swapped/empty variable and a while loop (maybe do/while)
+
+function sort(arr) {
+  let swapped;
+  do {} while (swapped);
+  return arr;
+}
+
+sort([]); // []
+sort([-4, 1, Infinity, 3, 3, 0]); // [-4, 0, 1, 3, 3, Infinity]

bccf1335a3464170a06d643fcdd847f4fca05254
Dale Holstein <holstein30@live.com>
2019-07-04 06:03:10 -0400

    includes setup

--- includes/app.js
+++ includes/app.js
@@ -1,4 +1,6 @@
 //  Return true or false indicating whether the given number appears in the given sorted array.
+// Can use includes method or a simple loop comparing values.
+// Probably an algorithm that can dramatically improve performance.
 
 const test = includes([1, 3, 8, 10], 8); // true
 const test2 = includes([1, 3, 8, 8, 15], 15); // true

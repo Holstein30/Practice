c8f34f7c34db71b7b15b64dc7c4606d644bf7abc
Dale Holstein <holstein30@live.com>
2019-07-04 06:04:41 -0400

    includes SOLVED - can be heavily improved

--- includes/app.js
+++ includes/app.js
@@ -2,6 +2,12 @@
 // Can use includes method or a simple loop comparing values.
 // Probably an algorithm that can dramatically improve performance.
 
+function includes(arr, num) {
+  return arr.includes(num);
+}
+
 const test = includes([1, 3, 8, 10], 8); // true
 const test2 = includes([1, 3, 8, 8, 15], 15); // true
 const test3 = includes([1, 3, 8, 10, 15], 9); // false
+
+console.log({ test, test2, test3 });

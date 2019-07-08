4e723b59f274f4bb56e0be36c66c9c247eadcdd1
Dale Holstein <holstein30@live.com>
2019-07-08 18:34:05 -0400

    smallest-multiple SOLVED

--- smallest-multiple/app.js
+++ smallest-multiple/app.js
@@ -2,8 +2,37 @@
 // 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 // What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
-// obviously need a loop. Likely a nested loop.
+// obviously need a loop. Maybe use a while loop and use brute force first
 // Can skip 1 and test for evenness for 2 so can start on 3
-// Can elimanate numbers ending in anything other than a multiples of 20 (so advance 20 at a time)
+// Can eliminate numbers ending in anything other than a multiples of 20 (so advance 20 at a time)
 
-function smallestMultiple() {}
+function smallestMultiple() {
+  let i = 20;
+  while (
+    i % 3 != 0 ||
+    i % 4 != 0 ||
+    i % 5 != 0 ||
+    i % 6 != 0 ||
+    i % 7 != 0 ||
+    i % 8 != 0 ||
+    i % 9 != 0 ||
+    i % 10 != 0 ||
+    i % 11 != 0 ||
+    i % 12 != 0 ||
+    i % 13 != 0 ||
+    i % 14 != 0 ||
+    i % 15 != 0 ||
+    i % 16 != 0 ||
+    i % 17 != 0 ||
+    i % 18 != 0 ||
+    i % 19 != 0 ||
+    i % 20 != 0
+  ) {
+    i += 20;
+  }
+  return i;
+}
+
+const answer = smallestMultiple();
+
+console.log(answer);

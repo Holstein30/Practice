8207e3a71785da273f5a47f08db0949208937f90
Dale Holstein <holstein30@live.com>
2019-07-09 06:37:11 -0400

    sum-square-difference setup

---
+++ sum-square-difference/app.js
@@ -0,0 +1,5 @@
+// Project Euler #6
+// Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+// Could use an array and map over the numbers.
+// 3 functions - sum of squares, square of sum, and difference.

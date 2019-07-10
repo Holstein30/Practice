19b3ec4140124c971832fbc4311a2b42c1d03c16
Dale Holstein <holstein30@live.com>
2019-07-10 12:53:18 -0400

    nth-prime SOLVED

--- nth-prime/app.js
+++ nth-prime/app.js
@@ -3,3 +3,34 @@
 
 // Function with a loop to go nth number passed in
 // use isPrime function to check and then push all numbers into an array until the array lenght is equal to num
+
+function nthPrime(num) {
+  const primeArray = [2];
+  let nthPrimeNum;
+  for (let i = 3; primeArray.length <= num; i += 2) {
+    if (isPrime(i)) {
+      primeArray.push(i);
+      if (primeArray.length === num) {
+        nthPrimeNum = i;
+      }
+    }
+  }
+  return nthPrimeNum;
+}
+
+let isPrime = factor => {
+  let prime = true;
+  for (let i = 2; i < factor; i++) {
+    if (factor % i === 0) {
+      prime = false;
+      break;
+    }
+  }
+  return prime;
+};
+
+const test = nthPrime(10001);
+
+console.log(test);
+
+// performance can be improved

0f5d7574af6ed59d16d7e626433f8c5791e290c6
Dale Holstein <holstein30@live.com>
2019-07-10 12:45:33 -0400

    nth-prime setup

---
+++ nth-prime/app.js
@@ -0,0 +1,5 @@
+// Project Euler Problem 7
+// What is the 10,001st prime number?
+
+// Function with a loop to go nth number passed in
+// use isPrime function to check and then push all numbers into an array until the array lenght is equal to num

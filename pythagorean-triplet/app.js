b146fdabc0751656abb350c39e0d129ec7ddabeb
Dale Holstein <holstein30@live.com>
2019-07-12 07:56:36 -0400

    determining if triplets satisfy pythag theorem

--- pythagorean-triplet/app.js
+++ pythagorean-triplet/app.js
@@ -1,2 +1,16 @@
 // Project Euler Problem 9
 // There exists exactly one Pythagorean triplet for which a + b + c = 1000. Find the product of abc
+
+// First lets write a simple function to determine if Pythagorean Theorem is met
+
+function isPytha(a, b, c) {
+  if (Math.pow(a, 2) + Math.pow(b, 2) === Math.pow(c, 2)) {
+    return true;
+  }
+  return false;
+}
+
+const pythBool = isPytha(3, 4, 5);
+const pythBool2 = isPytha(2, 3, 6);
+
+console.log({ pythBool, pythBool2 });

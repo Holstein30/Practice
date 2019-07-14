1fafbc9adbd8e8031892a65e6728be7dc08132a9
Dale Holstein <holstein30@live.com>
2019-07-14 10:09:14 -0400

    getting keys

--- equiv-properties/app.js
+++ equiv-properties/app.js
@@ -13,3 +13,11 @@ const obj2 = {
   uhhh: "uhh",
   name: "name"
 };
+
+function equivProps(a, b) {
+  const keys1 = Object.keys(a);
+  const keys2 = Object.keys(b);
+  console.log({ keys1, keys2 });
+}
+
+equivProps(obj1, obj2);

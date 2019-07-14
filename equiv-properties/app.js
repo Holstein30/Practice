249ef5b9e47b8786de6adf898a8cdf73b0c4ca47
Dale Holstein <holstein30@live.com>
2019-07-14 10:06:35 -0400

    equiv-properties setup

---
+++ equiv-properties/app.js
@@ -0,0 +1,15 @@
+// Write a JavaScript program to compare two objects to determine if the first one contains equivalent property values to the second one.
+
+const obj1 = {
+  name: "name",
+  age: 21,
+  height: "6",
+  weight: 200
+};
+
+const obj2 = {
+  hmmm: "hmm",
+  rawr: "rawr",
+  uhhh: "uhh",
+  name: "name"
+};

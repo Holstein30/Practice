a76de8ac8204f6902c8884384d7f2e4da1ebaa6b
Dale Holstein <holstein30@live.com>
2019-07-15 19:15:15 -0400

    digits-array SOLVED

--- digits-array/app.js
+++ digits-array/app.js
@@ -1,7 +1,9 @@
 // Write a JavaScript program to converts a specified number to an array of digits.
 
 const digitsArray = num => {
-  return [...`${num}`];
+  return [...`${num}`].map(n => parseInt(n));
 };
 
 console.log(digitsArray(10));
+console.log(digitsArray(1234));
+console.log(digitsArray(91230));

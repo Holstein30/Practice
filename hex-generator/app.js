bc5ca1bcfef24e4366e1882e36e8aa28fca2a5a4
Dale Holstein <holstein30@live.com>
2019-07-16 18:58:35 -0400

    hex-generator SOLVED

--- hex-generator/app.js
+++ hex-generator/app.js
@@ -1,1 +1,8 @@
 // Write a JavaScript program to generate a random hexadecimal color code.
+
+const hexCodeGenerator = () => {
+  let code = Math.floor(Math.random() * 16777215).toString(16);
+  return `#${code}`;
+};
+
+console.log(hexCodeGenerator());

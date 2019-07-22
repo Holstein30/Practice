722b186d8fc2dd1873e06bdee5db74569213ada3
Dale Holstein <holstein30@live.com>
2019-07-22 18:46:39 -0400

    converting string into array

--- leetcode-exercises/LC3_lengthOfLongestSubstring/index.js
+++ leetcode-exercises/LC3_lengthOfLongestSubstring/index.js
@@ -4,6 +4,11 @@
 // lengthOfLongestSubstring("abcabcbb") --> 3 since length of "abc"
 // lengthOfLongestSubstring("bbbbb") --> 1 since length of "b"
 
-function lengthOfLongestSubstring(s) {}
+function lengthOfLongestSubstring(s) {
+  const sArray = s.split("");
+  console.log(sArray);
+}
+
+lengthOfLongestSubstring("abcabcbb");
 
 module.exports = lengthOfLongestSubstring;

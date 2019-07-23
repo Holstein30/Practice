888f21e56dad722b257a27902436c01e92736f62
Dale Holstein <holstein30@live.com>
2019-07-23 18:52:35 -0400

    imported palindrome function for ints to modify for strings

--- leetcode-exercises/LC5_longestPalindrome/index.js
+++ leetcode-exercises/LC5_longestPalindrome/index.js
@@ -4,6 +4,18 @@
 // longestPalindrome("abba") --> "abba"
 // longestPalindrome("a") --> "a"
 
-function longestPalindrome(s) {}
+function longestPalindrome(s) {
+  const isPalindrome = num => {
+    const numArray = num.toString().split("");
+    const reverseNumArray = numArray.slice().reverse();
+    let palidromeBool = true;
+    for (let i = 0; i < numArray.length; i++) {
+      if (numArray[i] != reverseNumArray[i]) {
+        palidromeBool = false;
+      }
+    }
+    return palidromeBool;
+  };
+}
 
 module.exports = longestPalindrome;

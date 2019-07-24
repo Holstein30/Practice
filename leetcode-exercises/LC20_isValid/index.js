9cb9bdd5804b3456e92fec38de1c5d2a08cf3ef0
Dale Holstein <holstein30@live.com>
2019-07-24 14:18:16 -0400

    loop created to check brackets and push to stack

--- leetcode-exercises/LC20_isValid/index.js
+++ leetcode-exercises/LC20_isValid/index.js
@@ -1,6 +1,16 @@
 const isValid = s => {
   let stack = [];
   let pairsHashMap = { "(": ")", "{": "}", "[": "]" };
+
+  for (let i = 0; i < s.length; i++) {
+    let char = s[i];
+
+    if (pairsHashMap[char]) {
+      stack.push(char);
+    } else if (pairsHashMap[stack.pop()] !== char) {
+      return false;
+    }
+  }
 };
 
 module.exports = isValid;

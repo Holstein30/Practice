4bb10414b7bbca65fdb6480d3d007e8a9413b955
Dale Holstein <holstein30@live.com>
2019-07-24 14:15:50 -0400

    stack and pairsHashMap added

--- leetcode-exercises/LC20_isValid/index.js
+++ leetcode-exercises/LC20_isValid/index.js
@@ -1,44 +1,6 @@
 const isValid = s => {
-  const splitArray = s.split("");
-  const bracesArray = splitArray.filter(
-    element =>
-      element === "{" ||
-      element === "}" ||
-      element === "[" ||
-      element === "]" ||
-      element === "(" ||
-      element === ")"
-  );
-  if (bracesArray.length % 2 != 0) {
-    return false;
-  }
-  let startStack = [];
-  let balancedBool = false;
-  const balanced = bracesArray.forEach(element => {
-    if (element === "{" || element === "(" || element === "[") {
-      startStack.push(element);
-    }
-    if (element === "}") {
-      if (startStack.pop() === "{") {
-        balancedBool = true;
-      } else {
-        balancedBool = false;
-      }
-    } else if (element === ")") {
-      if (startStack.pop() === "(") {
-        balancedBool = true;
-      } else {
-        balancedBool = false;
-      }
-    } else if (element === "]") {
-      if (startStack.pop() === "[") {
-        balancedBool = true;
-      } else {
-        balancedBool = false;
-      }
-    }
-  });
-  return balancedBool;
+  let stack = [];
+  let pairsHashMap = { "(": ")", "{": "}", "[": "]" };
 };
 
 module.exports = isValid;

d14913936a4cd008525af33dd12da294a7987ab8
Dale Holstein <holstein30@live.com>
2019-07-24 14:11:03 -0400

    LC20 starting solution with some bugs

--- leetcode-exercises/LC20_isValid/index.js
+++ leetcode-exercises/LC20_isValid/index.js
@@ -1,3 +1,44 @@
-const isValid = s => {};
+const isValid = s => {
+  const splitArray = s.split("");
+  const bracesArray = splitArray.filter(
+    element =>
+      element === "{" ||
+      element === "}" ||
+      element === "[" ||
+      element === "]" ||
+      element === "(" ||
+      element === ")"
+  );
+  if (bracesArray.length % 2 != 0) {
+    return false;
+  }
+  let startStack = [];
+  let balancedBool = false;
+  const balanced = bracesArray.forEach(element => {
+    if (element === "{" || element === "(" || element === "[") {
+      startStack.push(element);
+    }
+    if (element === "}") {
+      if (startStack.pop() === "{") {
+        balancedBool = true;
+      } else {
+        balancedBool = false;
+      }
+    } else if (element === ")") {
+      if (startStack.pop() === "(") {
+        balancedBool = true;
+      } else {
+        balancedBool = false;
+      }
+    } else if (element === "]") {
+      if (startStack.pop() === "[") {
+        balancedBool = true;
+      } else {
+        balancedBool = false;
+      }
+    }
+  });
+  return balancedBool;
+};
 
 module.exports = isValid;

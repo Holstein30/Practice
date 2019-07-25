75f8dac22d7f00ee19e4fa6ebe03f2764137d6bb
Dale Holstein <holstein30@live.com>
2019-07-25 12:30:48 -0400

    for look for splitting, sorting, and rejoining strings

--- leetcode-exercises/LC49_groupAnagrams/index.js
+++ leetcode-exercises/LC49_groupAnagrams/index.js
@@ -1,5 +1,13 @@
 function groupAnagrams(strs) {
   let grouped = {};
+
+  for (let i = 0; i < strs.length; i++) {
+    const word = strs[i];
+    const key = word
+      .split("")
+      .sort()
+      .join("");
+  }
 }
 
 module.exports = groupAnagrams;

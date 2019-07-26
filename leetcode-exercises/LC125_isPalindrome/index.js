17d39cb76dded0c1d2ecc59d2a4392a4f1781df7
Dale Holstein <holstein30@live.com>
2019-07-26 15:32:32 -0400

    testing if a single string is a palindrome

--- leetcode-exercises/LC125_isPalindrome/index.js
+++ leetcode-exercises/LC125_isPalindrome/index.js
@@ -1,3 +1,13 @@
-function isPalindrome(s) {}
+function isPalindrome(s) {
+  const stringArray = s.split("");
+  const reverseStringArray = stringArray.slice().reverse();
+  let palidromeBool = true;
+  for (let i = 0; i < stringArray.length; i++) {
+    if (stringArray[i] != reverseStringArray[i]) {
+      palidromeBool = false;
+    }
+  }
+  return palidromeBool;
+}
 
 module.exports = isPalindrome;

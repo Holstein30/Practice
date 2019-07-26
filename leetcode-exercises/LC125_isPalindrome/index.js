1a258c13a193fb91def83c7d619e9c0c371d556f
Dale Holstein <holstein30@live.com>
2019-07-26 15:38:08 -0400

    returning false if sides don't mirror each other

--- leetcode-exercises/LC125_isPalindrome/index.js
+++ leetcode-exercises/LC125_isPalindrome/index.js
@@ -1,6 +1,15 @@
 function isPalindrome(s) {
   // Sanitize the input string
   s = s.toLowerCase().replace(/[\W_]/g, "");
+
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    if (s[left] !== s[right]) {
+      return false;
+    }
+  }
 }
 
 module.exports = isPalindrome;

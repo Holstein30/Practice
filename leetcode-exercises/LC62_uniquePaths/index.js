04a9bd64ecbcebce97a0f942414aa865b8caedf9
Dale Holstein <holstein30@live.com>
2019-07-29 19:03:06 -0400

    filling out first row of matrix

--- leetcode-exercises/LC62_uniquePaths/index.js
+++ leetcode-exercises/LC62_uniquePaths/index.js
@@ -1,8 +1,12 @@
 function uniquePaths(m, n) {
   const dpMatrix = [];
 
-  for (let i = 0; row <= n; row++) {
+  for (let row = 0; row <= n; row++) {
     dpMatrix.push([]);
+  }
+
+  for (let row = 0; row < n; row++) {
+    dpMatrix[row][0] = 1;
   }
 }
 

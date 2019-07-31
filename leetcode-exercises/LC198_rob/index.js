2ab81246788e7a44b4e3eb381b1a3eadd61d6528
Dale Holstein <holstein30@live.com>
2019-07-31 13:29:49 -0400

    maxLoot variable initilaized

--- leetcode-exercises/LC198_rob/index.js
+++ leetcode-exercises/LC198_rob/index.js
@@ -2,6 +2,8 @@ function rob(nums) {
   if (nums.length === 0) return 0;
   if (nums.length === 1) return nums[0];
   if (nums.length === 2) return Math.max(nums[0], nums[1]);
+
+  let maxLootAtNth = [nums[0], Math.max(nums[0], nums[1])];
 }
 
 module.exports = rob;

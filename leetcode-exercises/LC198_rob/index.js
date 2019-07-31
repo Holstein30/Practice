0ab1b014523e9e8d9c22df06f9720f99848916a9
Dale Holstein <holstein30@live.com>
2019-07-31 13:31:37 -0400

    looking through and testing for max loot

--- leetcode-exercises/LC198_rob/index.js
+++ leetcode-exercises/LC198_rob/index.js
@@ -4,6 +4,12 @@ function rob(nums) {
   if (nums.length === 2) return Math.max(nums[0], nums[1]);
 
   let maxLootAtNth = [nums[0], Math.max(nums[0], nums[1])];
+
+  for (let i = 2; i < nums.length; i++) {
+    maxLootAtNth.push(
+      Math.max(nums[i] + maxLootAtNth[i - 2], maxLootAtNth[i - 1])
+    );
+  }
 }
 
 module.exports = rob;

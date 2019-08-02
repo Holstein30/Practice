a58becf2598c804fbc4e14d4d15203f901abab64
Dale Holstein <holstein30@live.com>
2019-08-02 17:18:33 -0400

    initialize loops and coinValue

--- leetcode-exercises/LC322_coinChange/index.js
+++ leetcode-exercises/LC322_coinChange/index.js
@@ -1,6 +1,12 @@
 function coinChange(coins, amount) {
   let dpMinCoins = new Array(amount + 1).fill(Infinity);
   dpMinCoins[0] = 0;
+
+  for (let i = 1; i < dpMinCoins.length; i++) {
+    for (let j = 0; j < coins.length; j++) {
+      const coinValue = coins[j];
+    }
+  }
 }
 
 module.exports = coinChange;

74d4cb687b4433b8d07f85cdeaf92702376f1612
Dale Holstein <holstein30@live.com>
2019-08-03 08:57:39 -0400

    checking if intervals overlap

--- leetcode-exercises/LC56_merge/index.js
+++ leetcode-exercises/LC56_merge/index.js
@@ -8,6 +8,10 @@ const merge = intervals => {
   for (let i = 0; i < intervals.length; i++) {
     const currentInterval = intervals[i];
     const lastInterval = res[res.length - 1];
+
+    if (currentInterval[0] <= lastInterval[1]) {
+      lastInterval[1] = Math.max(currentInterval[1], lastInterval[1]);
+    }
   }
 };
 

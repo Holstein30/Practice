794842fbdffbd49c548dd55e5b3f6954c02ed90b
Dale Holstein <holstein30@live.com>
2019-08-03 08:56:48 -0400

    looping over array and creating current and last interval for comparisions

--- leetcode-exercises/LC56_merge/index.js
+++ leetcode-exercises/LC56_merge/index.js
@@ -4,6 +4,11 @@ const merge = intervals => {
   intervals.sort((a, b) => a[0] - b[0]);
 
   const res = [intervals[0]];
+
+  for (let i = 0; i < intervals.length; i++) {
+    const currentInterval = intervals[i];
+    const lastInterval = res[res.length - 1];
+  }
 };
 
 module.exports = merge;

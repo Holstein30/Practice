2ccf7e0e0370c18e1f094b3ba4e00fe4ea3ceafb
Dale Holstein <holstein30@live.com>
2019-08-05 18:11:30 -0400

    looping over interval and setting current, start, and end intervals

--- leetcode-exercises/LC435_eraseOverlapIntervals/index.js
+++ leetcode-exercises/LC435_eraseOverlapIntervals/index.js
@@ -4,6 +4,14 @@ const eraseOverlapIntervals = intervals => {
   let count = 0;
 
   intervals.sort((a, b) => a[0] - b[0]);
+
+  let end = intervals[0][1];
+
+  for (let i = 1; i < intervals.length; i++) {
+    const interval = intervals[i];
+    const intervalStart = interval[0];
+    const intervalEnd = interval[1];
+  }
 };
 
 module.exports = eraseOverlapIntervals;

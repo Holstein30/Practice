d3863b69786a964037b8e7854b1648f9d04c2aaf
Dale Holstein <holstein30@live.com>
2019-08-07 08:10:54 -0400

    looping and comparing linked list values for merging

--- leetcode-exercises/LC21_mergeTwoLists/index.js
+++ leetcode-exercises/LC21_mergeTwoLists/index.js
@@ -2,6 +2,14 @@ function mergeTwoLists(l1, l2) {
   const dummyHead = { next: null };
 
   let tail = dummyHead;
+
+  while (l1 && l2) {
+    if (l1.val < l2.val) {
+      tail.next = l1;
+      tail = tail.next;
+      li = li.next;
+    }
+  }
 }
 
 module.exports = mergeTwoLists;

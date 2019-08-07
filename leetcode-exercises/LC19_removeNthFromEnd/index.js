e7d02a972086bbf682dbca67b2d3ede7d4ddc778
Dale Holstein <holstein30@live.com>
2019-08-06 20:21:24 -0400

    moving fast N nodes ahead of slow

--- leetcode-exercises/LC19_removeNthFromEnd/index.js
+++ leetcode-exercises/LC19_removeNthFromEnd/index.js
@@ -2,6 +2,11 @@ function removeNthFromEnd(head, n) {
   let dummyHead = { next: head };
   let slow = dummyHead;
   let fast = dummyHead;
+
+  // Move fast N nodes ahead of slow
+  for (let i = 1; i <= n; i++) {
+    fast = fast.next;
+  }
 }
 
 module.exports = removeNthFromEnd;

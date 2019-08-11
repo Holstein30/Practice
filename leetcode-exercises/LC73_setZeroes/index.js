b42a82021e9a176d65504967785b0d1e9d113c71
Dale Holstein <holstein30@live.com>
2019-08-11 09:08:34 -0400

    testing if first row/column has a zero and if so setting flag vars to true

--- leetcode-exercises/LC73_setZeroes/index.js
+++ leetcode-exercises/LC73_setZeroes/index.js
@@ -1,6 +1,22 @@
 function setZeroes(matrix) {
   let firstColHasZero = false;
   let firstRowHasZero = false;
+
+  // Check if first column has zero
+  for (let i = 0; i < matrix.length; i++) {
+    if (matrix[i][0] === 0) {
+      firstColHasZero = true;
+      break;
+    }
+  }
+
+  // Check if first row has zero
+  for (let i = 0; i < matrix[0].length; i++) {
+    if (matrix[0][i] === 0) {
+      firstRowHasZero = true;
+      break;
+    }
+  }
 }
 
 module.exports = setZeroes;

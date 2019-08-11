7804124afaf5b93698f10c8e9977f2ae6b124b60
Dale Holstein <holstein30@live.com>
2019-08-10 22:19:07 -0400

    initilize spiral array and catch edge case

--- leetcode-exercises/LC54_spiralOrder/index.js
+++ leetcode-exercises/LC54_spiralOrder/index.js
@@ -1,3 +1,9 @@
-function spiralOrder(matrix) {}
+function spiralOrder(matrix) {
+  const spiralArray = [];
+  if (matrix.length === 0) {
+    return spiralArray;
+    ß;
+  }
+}
 
 module.exports = spiralOrder;

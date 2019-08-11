37e7d8d40e6ea8f463248726e121c9e1b1106c7c
Dale Holstein <holstein30@live.com>
2019-08-10 22:20:01 -0400

    initialize directions

--- leetcode-exercises/LC54_spiralOrder/index.js
+++ leetcode-exercises/LC54_spiralOrder/index.js
@@ -2,8 +2,13 @@ function spiralOrder(matrix) {
   const spiralArray = [];
   if (matrix.length === 0) {
     return spiralArray;
-    ß;
   }
+
+  let top = 0;
+  let bottom = matrix.length - 1;
+  let left = 0;
+  let right = matrix[0].length - 1;
+  let dir = "right";
 }
 
 module.exports = spiralOrder;

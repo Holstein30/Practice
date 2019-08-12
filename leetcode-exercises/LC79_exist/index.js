fceaa219dff8fd1ced27010040091ef35c3a7f49
Dale Holstein <holstein30@live.com>
2019-08-12 17:53:41 -0400

    looping through entire grid

--- leetcode-exercises/LC79_exist/index.js
+++ leetcode-exercises/LC79_exist/index.js
@@ -1,5 +1,13 @@
 function exist(board, word) {
   let found = false;
+
+  for (let row = 0; row < board.length; row++) {
+    for (let col = 0; col < board[0].length; col++) {
+      if (board[row][col] === word[0]) {
+        defaultStatus(row, col, 0, word);
+      }
+    }
+  }
 }
 
 module.exports = exist;

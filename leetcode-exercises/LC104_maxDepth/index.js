f3f5fcf16bcbe6676a66152ee0134e4dccc5b14c
Dale Holstein <holstein30@live.com>
2019-08-16 11:40:54 -0400

    div helper function created - calculating maxDepth if null

--- leetcode-exercises/LC104_maxDepth/index.js
+++ leetcode-exercises/LC104_maxDepth/index.js
@@ -1,5 +1,11 @@
 function maxDepth(root) {
   let maxDepth = 0;
+
+  function dive(node, currentDepth) {
+    if (!node) {
+      maxDepth = Math.max(currentDepth - 1, maxDepth);
+    }
+  }
 }
 
 module.exports = maxDepth;

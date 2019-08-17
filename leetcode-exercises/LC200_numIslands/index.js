58ffb6dbb767f6767ca552ec3ea0fc801626a9f5
Dale Holstein <holstein30@live.com>
2019-08-17 08:31:24 -0400

    iterate through grid

--- leetcode-exercises/LC200_numIslands/index.js
+++ leetcode-exercises/LC200_numIslands/index.js
@@ -1,5 +1,9 @@
 function numIslands(grid) {
   let count = 0;
+
+  for (let row = 0; row < grid.length; row++) {
+    for (let col = 0; col < grid[0].length; col++) {}
+  }
 }
 
 module.exports = numIslands;

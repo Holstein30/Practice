2071839c96d54358935250e260d4e8667af927f2
Dale Holstein <holstein30@live.com>
2019-08-17 08:32:15 -0400

    determine if tile is landk

--- leetcode-exercises/LC200_numIslands/index.js
+++ leetcode-exercises/LC200_numIslands/index.js
@@ -2,7 +2,12 @@ function numIslands(grid) {
   let count = 0;
 
   for (let row = 0; row < grid.length; row++) {
-    for (let col = 0; col < grid[0].length; col++) {}
+    for (let col = 0; col < grid[0].length; col++) {
+      if (grid[row][col] === "1") {
+        count++;
+        dfs(grid, row, col);
+      }
+    }
   }
 }
 

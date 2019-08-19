16db934562e33005eda074eeaa273f5d383646d3
Dale Holstein <holstein30@live.com>
2019-08-19 10:39:12 -0400

    created temp var for swap

--- leetcode-exercises/LC226_invertTree/index.js
+++ leetcode-exercises/LC226_invertTree/index.js
@@ -2,8 +2,9 @@ function invertTree(root) {
   function helper(node) {
     if (!node) return;
 
+    const temp = node.left;
     node.left = node.right;
-    node.right = node.left;
+    node.right = temp;
   }
 }
 

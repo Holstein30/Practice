32003609962501c869191d484837f592a56e9de6
Dale Holstein <Holstein30@live.com>
2020-01-11 13:24:57 -0500

    LC217 - conditional

--- leetcode-exercises/LC217_containsDuplicate/index.js
+++ leetcode-exercises/LC217_containsDuplicate/index.js
@@ -2,8 +2,15 @@ const containsDuplicate = nums => {
     const visitedNums = {}; // {7: true, 2: true};
 
     for (let i = 0; i < nums.length; i++) {
-        const nums = nums[i];
+        const num = visitedNums[i];
+
+        if (visitedNums[num]) {
+            return true;
+        } else {
+            visitedNums[num] = true;
+        }
     }
+    return false;
 };
 
 module.exports = containsDuplicate;

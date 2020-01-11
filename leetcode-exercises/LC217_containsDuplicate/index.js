519bf79111f18e8810293b40dd33f4723975267b
Dale Holstein <Holstein30@live.com>
2020-01-11 13:27:14 -0500

    LC217 - SOLVED

--- leetcode-exercises/LC217_containsDuplicate/index.js
+++ leetcode-exercises/LC217_containsDuplicate/index.js
@@ -2,7 +2,7 @@ const containsDuplicate = nums => {
     const visitedNums = {}; // {7: true, 2: true};
 
     for (let i = 0; i < nums.length; i++) {
-        const num = visitedNums[i];
+        const num = nums[i];
 
         if (visitedNums[num]) {
             return true;
